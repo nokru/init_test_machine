cf4a47d370fce5578ecf65d62b7997a532f82311
Lukas Waldenberger <l.waldenberger@gmail.com>
2018-11-06 23:25:52 +0100

    add make files

---
+++ make/docker.mk
@@ -0,0 +1,13 @@
+$(eval $(call defw,DOCKER,docker))
+
+.PHONY: du
+du:: ##@Docker System disk usage
+  @sudo su -c '$(DOCKER) system df -v'
+
+.PHONY: prune
+prune:: ##@Docker Prune docker all
+  @sudo su -c '$(DOCKER) system prune -a -f'
+
+.PHONY: pruneall
+pruneall:: ##@Docker Prune docker system and images
+  @sudo su -c '$(DOCKER) system prune -a -f  && $(DOCKER) image prune -f && $(DOCKER) volume prune -f && docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi'
