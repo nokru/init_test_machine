b25865206825b051db3f02c67dd96da27a633d55
Lukas Waldenberger <l.waldenberger@gmail.com>
2018-11-06 23:32:03 +0100

    typo makefile

--- make/apache.mk
+++ make/apache.mk
@@ -10,11 +10,11 @@ install-mysql:: ##@Build Install MySQL/MariaDB
 	apt-get install -y mariadb-client mariadb-server
 
 .PHONY: install-php
-install-mysql:: ##@Build Install PHP7
+install-php:: ##@Build Install PHP7
 	apt-get install -y php7.0 php7.0-mysql
 
 .PHONY: install-phpmyadmin
-install-mysql:: ##@Build Install phpmyadmin
+install-phpmyadmin:: ##@Build Install phpmyadmin
 	apt-get install -y phpmyadmin
 
 .PHONY: install-lamp
