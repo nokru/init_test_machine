63205fc1df209e036bb78c9f37e182b07b31c31f
Lukas Waldenberger <l.waldenberger@gmail.com>
2018-11-06 07:53:00 +0100

    key integration

--- initscript.sh
+++ initscript.sh
@@ -1,6 +1,6 @@
 #
-#	get this file by executing as root
-#   wget -qO- https://raw.githubusercontent.com/nokru/init_test_machine/master/initscript.sh | bash
+#  get this file by executing as root
+#  wget -qO- https://raw.githubusercontent.com/nokru/init_test_machine/master/initscript.sh | bash
 #
 
 apt-get install -y git curl
@@ -11,4 +11,15 @@ git clone https://github.com/nokru/init_test_machine.git init_scripts
 
 cd init_scripts
 
+#key encryption
+# tar cz keys | \
+#      openssl enc -aes-256-cbc -e > keys.tar.gz.enc
+#is standard password
+
+openssl enc -aes-256-cbc -d -in keys.tar.gz.enc | tar xz
+
+cd keys
+cp * /root/.ssh/
+
+service ssh restart
 
