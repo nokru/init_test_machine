d63427033ec0afb9fb404736c678cf4280cdaebb
Lukas Waldenberger <l.waldenberger@gmail.com>
2018-11-06 07:37:54 +0100

    update url

--- initscript.sh
+++ initscript.sh
@@ -1,10 +1,14 @@
 #
 #	get this file by executing as root
-#   curl -fsSL  | bash
+#   curl -fsSL  https://raw.githubusercontent.com/nokru/init_test_machine/master/initscript.sh | bash
 #
 
 apt-get install -y git
 
 cd /tmp
 
-git clone git@github.com/
+git clone git@github.com:nokru/init_test_machine.git init_scripts
+
+cd init_scripts
+
+
