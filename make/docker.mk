7fcf85ec6959fe4a9ae5e4f0dc9483e406cfad87
Lukas Waldenberger <l.waldenberger@gmail.com>
2018-11-07 06:58:02 +0100

    escape docker-compose

--- make/docker.mk
+++ make/docker.mk
@@ -19,7 +19,7 @@ install-docker:: ##@Docker Install Docker
 
 .PHONY: install-docker-compose
 install-docker-compose:: ##@Docker Install docker-compose
-	@curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	@curl -L \"https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose
 	@chmod +x /usr/local/bin/docker-compose
 
 		
