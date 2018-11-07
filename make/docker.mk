cd5ebe95509227bae7d20a071218ac13a4fc9851
Lukas Waldenberger <l.waldenberger@gmail.com>
2018-11-07 06:55:24 +0100

    add docker-compose

--- make/docker.mk
+++ make/docker.mk
@@ -13,5 +13,13 @@ pruneall:: ##@Docker Prune docker system and images
 	@sudo su -c '$(DOCKER) system prune -a -f  && $(DOCKER) image prune -f && $(DOCKER) volume prune -f && docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi'
 
 .PHONY: install-docker
-install-docker:: ##@Docker Prune docker system and images
-	wget -qO- https://get.docker.com | bash
+install-docker:: ##@Docker Install Docker
+	@wget -qO- https://get.docker.com | bash
+
+
+.PHONY: install-docker-compose
+install-docker-compose:: ##@Docker Install docker-compose
+	@curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	@chmod +x /usr/local/bin/docker-compose
+
+		
